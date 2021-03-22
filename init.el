227d2411730475395135f83cfc6c84d0bc5b3712
Dan Barbarito <dan@barbarito.me>
2021-03-21 22:47:52 -0400

    journal mode

--- init.el
+++ init.el
@@ -146,7 +146,14 @@
        nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org
+        +dragndrop
+        +pomodoro
+        +pandoc
+        +journal
+        +gnuplot
+        +jupyter
+        +present)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
